6de35bf8f13eefd47d357ae5171d3723f854cd72
Luannunes02 <nunesesbaltar.luan02@gmail.com>
2022-12-01 07:16:46 -0300

    inserindo animacao do robo

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,6 +1,8 @@
 import { View, Text, ScrollView, Image, StyleSheet } from 'react-native'
 import React from 'react'
 
+import LifeStatus from '../../components/common/LifeStatus'
+
 export default function Start() {
     return (
         <View style={styles.container}>
@@ -10,6 +12,7 @@ export default function Start() {
                         source={require('../../assets/icons/logo3.png')}
                         style={styles.logo}
                     />
+                    <LifeStatus />
                     <Text style={styles.description}>Vamos transformar sua vida {'\n'}em um jogo, buscando sempre {'\n'} {" "} o melhor nivel.</Text>
 
                 </View>
